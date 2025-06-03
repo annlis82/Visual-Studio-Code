af8e5220edbd383e5f4f44ac58a665ac628da277
annlis82 <almangseth@gmail.com>
2025-06-03 15:09:35 +0200

    Added const form and const button

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener(DOMContentLoaded), 
+function () {
+  const form = document.getElementById
+  ("contact-form");
+
+  const button =
+  document.getElementById
+  ("contact_form_submit_button");
+}
