5e9b4f821fcae36a8a42c48b25031eebc58629b8
annlis82 <almangseth@gmail.com>
2025-06-05 10:47:39 +0200

    Test on hamburger icon

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 function toggleMenu() {
   const menu = document.getElementById('menu');
+  const hamburger = document.querySelector('.hamburger');
+
   menu.classList.toggle('open');
+  hamburger.classList.toggle('open'); 
 }
+
