4ed8433e87ab13255b7b7d3e30d10d5e1ec4bb3b
annlis82 <almangseth@gmail.com>
2025-06-05 14:30:34 +0200

    Pasted the form js back in the code

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 console.log("JavaScript is connected!");
+
+document.addEventListener("DOMContentLoaded", 
+  function () {
+    const form = document.getElementById
+    ("contact-form");
+  
+    const button =
+    document.getElementById
+    ("contact_form_submit_button");
+  
+    form.addEventListener ("submit",
+      function(event) {
+        event.preventDefault();
+  
+      const firstName = document.getElementById
+      ("first_name").value;
+      const email = document.getElementById
+      ("email").value;
+  
+     if (email) {
+      button.textContent = "sending";
+      button.disabled = "true";
+  
+      setTimeout (() => {
+        button.setAttribute ("style", "display:none;");
+  
+        const el = document.createElement("p")
+        el.innerHTML = "Thank you for signing up to our newsletter";
+        form.appendChild(el);
+      }, 3000);
+       } 
+      form.reset();
+     });
+  });
+  
